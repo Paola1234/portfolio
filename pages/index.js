425c9f7ffa963d4a16fe2607459d0ca420385b78
estebandalelr <estebandalelr@gmail.com>
2020-11-15 13:20:31 -0600

    Correct grammar

--- pages/index.js
+++ pages/index.js
@@ -7,7 +7,7 @@ export default function index() {
   return (
     <Layout home>
       <Head>
-        <title>Paola Latino Portfolio</title>
+        <title>Paola Latino's Portfolio</title>
       </Head>
       <section className={utilStyles.headingMd}>
         <p>Passion for Tech + Design. Completely passionate to improve people's experience in tech through my ideas with UX/UI design.</p>
