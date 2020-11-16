d440cec78582247fc70d5945623b3bf13cbcd398
paola1234 <paolalat188@gmail.com>
2020-11-16 10:24:59 -0500

    Fixed errored merged

--- pages/mindvalley.js
+++ pages/mindvalley.js
@@ -9,12 +9,12 @@ export default function mindvalley() {
           <title>Mindvalley</title>
         </Head>
         <section className={utilStyles.headingMd}>
-          <img src="/images/mindvalleylogo.svg"></img>
+          <img src="/images/mv-logo.svg"></img>
           <h2 className="center-title"> Why Mindvalley?</h2>
         </section>
         <section>
         <div>
-              <p>I want to join because I want to achieve...</p>
+              <p>I felt motivated to apply because I've seen how your company culture can</p>
               <p>I really hope to hear from you soon</p>
           </div>
         </section>
