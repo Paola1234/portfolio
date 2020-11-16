4c48a8637696b324773217928e02c5142db119ae
estebandalelr <estebandalelr@gmail.com>
2020-11-16 10:26:56 -0600

    Fix double declaration

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,4 @@
 import Head from 'next/head'
-import Layout, { siteTitle } from '../components/layout'
 import Layout from '../components/layout.js'
 import utilStyles from '../styles/utils.module.css'
 
