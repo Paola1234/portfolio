164acd60d1695e0069cf91ec2990d89841c219dd
paola1234 <paolalat188@gmail.com>
2020-11-16 12:21:05 -0500

    About me Page  css

--- pages/aboutme.js
+++ pages/aboutme.js
@@ -12,19 +12,19 @@ export default function aboutme() {
       </Head>
       <section className={utilStyles.headingMd}>
         <h2 className="center-title"> About Me </h2>
-        <span className="">Let me tell you a few facts about me</span>
-        <p>
+        <p className="text-center mb-4">Let me tell you a few facts about me</p>
+        <p className="mb-4">
         I’m a computer engineer and designer whose 
         love for tech and creativity who questions everyday “How can I make people’s life easier?”
         </p>
-        <p>
+        <p className="mb-4">
         That’s why I create experiences through tech. I'm continuously learning about topics related 
         to UX, gamification, mobile design, and innovative ways to improve people's experience.
         </p>
         <div className="row">
             <div className="column_equal">
                 <h3 className="purple-bold-text">Hard Skills</h3>
-                <ul>
+                <ul className="list-disc">
                     <li>Adobe XD</li>
                     <li>Adobe Illustrator</li>
                     <li>Adobe Photoshop</li>
@@ -44,7 +44,7 @@ export default function aboutme() {
             </div>
             <div className="column_equal">
             <h3 className="purple-bold-text">Soft Skills</h3>
-                <ul>
+                <ul className="list-disc">
                     <li>Analytic</li>
                     <li>Always learning</li>
                     <li>Creative</li>                    
@@ -61,8 +61,11 @@ export default function aboutme() {
       </section>
       <section>
             <h3 className="center-text">If you want to know more about my experience</h3>
-            <button></button>
-
+            <a href="https://drive.google.com/file/d/17eth5n0I086cch3-ZM6cdYjCRmnh0F89/view?usp=sharing">
+              <button className=" m-4 right-auto left-auto bg-purple-700 hover:bg-purple-500 text-white font-bold py-2 px-4 rounded">
+               Download my CV
+              </button>
+            </a>
       </section>
       <section>
             <h3 className="center-text">Contact me here</h3>
