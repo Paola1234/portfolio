d918fd70e9f9085e8822d148d731dc1bd3ce66ad
paola1234 <paolalat188@gmail.com>
2020-11-16 15:00:07 -0500

    Centered images

--- pages/projects.js
+++ pages/projects.js
@@ -14,7 +14,7 @@ export default function projects() {
       </section>
       <section>
         <div className="storibckg py-16" >
-          <img className="flex justify-around img-small" src="/icons/projectlogos/stori.png" alt=""/>
+          <img className="flex justify-around img-small center-img" src="/icons/projectlogos/stori.png" alt=""/>
           <h2 className="white-center-text">Gamification and rewards using a credit card</h2>
           <div className="flex justify-around mt-4 px-16">
             <a href="/stori"><button className="bg-transparent hover:bg-white-500 text-white
@@ -26,12 +26,12 @@ export default function projects() {
           </div>
           <p className="white-center-text mt-4">Mobile app</p>
           <p className="white-center-text mb-4">Gamification</p>
-          <img className="center img-small" src="/images/projectsImage/cardsStori.svg" alt=""/>
+          <img className="center img-small center-img" src="/images/projectsImage/cardsStori.svg" alt=""/>
         </div>
       </section>
       <section>
         <div className="estereobckg">
-          <img className="flex justify-around img-small" src="/icons/projectlogos/estereo.png" alt=""/>
+          <img className="flex justify-around img-small center-img" src="/icons/projectlogos/estereo.png" alt=""/>
           <h2 className="white-center-text">A new Festival experience during COVID-19 times</h2>
           <div className="flex justify-around mt-4 px-16">
             <a href="/estereo"><button className="bg-transparent hover:bg-white-500 text-white
@@ -43,7 +43,7 @@ export default function projects() {
           </div>
             <p className="white-center-text mt-4">Responsive Design</p>
             <p className="white-center-text mb-4">Mobile First</p>
-          <img className="center img-small" src="/images/projectsImage/estereoMockup.png" alt=""/>
+          <img className="center img-small center-img" src="/images/projectsImage/estereoMockup.png" alt=""/>
         </div>
       </section>
     </Layout>
