cac9fc0c8415e4830d65cce97df97b7d30e2439e
paola1234 <paolalat188@gmail.com>
2020-11-16 10:30:21 -0500

    Fixed errored merge 2 and mdx projects

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,5 @@
 import Head from 'next/head'
 import Layout, { siteTitle } from '../components/layout'
-import Layout from '../components/layout.js'
 import utilStyles from '../styles/utils.module.css'
 
 
