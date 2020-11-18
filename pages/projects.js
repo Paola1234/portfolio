e7aeb69cf04e8a0f05bdee8e948cac66e6ab11b2
paola1234 <paolalat188@gmail.com>
2020-11-17 20:45:07 -0500

    Responsive Sections Projects

--- pages/projects.js
+++ pages/projects.js
@@ -16,9 +16,9 @@ export default function projects() {
         <div className="py-16 storibckg" >
           <img className="flex justify-around img-small center-img" src="/icons/projectlogos/stori.png" alt=""/>
           <h2 className="white-center-text">Gamification and rewards using a credit card</h2>
-          <div className="flex justify-around px-16 mt-4">
-            <a href="/stori# Process"><button className="px-2 py-1 font-semibold text-white bg-transparent border rounded hover:bg-white-500 hover:text-white border-white-700 hover:border-transparent">Design Process</button></a>
-            <a href="/stori# Main problem"><button className="px-2 py-1 font-semibold text-white bg-transparent border rounded hover:bg-white-500 hover:text-white border-white-700 hover:border-transparent">Problem</button></a>
+          <div className="flex flex-wrap justify-around px-16 mt-4">
+            <a href="/stori# Process"><button className="px-2 py-1 mb-2 font-semibold text-white bg-transparent border rounded hover:bg-white-500 hover:text-white border-white-700 hover:border-transparent"> Process</button></a>
+            <a href="/stori# Main problem"><button className="px-2 py-1 mb-2 font-semibold text-white bg-transparent border rounded hover:bg-white-500 hover:text-white border-white-700 hover:border-transparent">Problem</button></a>
             <a href="/stori# Wireframes"><button className="px-2 py-1 font-semibold text-white bg-transparent border rounded hover:bg-white-500 hover:text-white border-white-700 hover:border-transparent">Wireframes</button></a>
           </div>
           <p className="mt-4 white-center-text">Mobile app</p>
@@ -30,9 +30,9 @@ export default function projects() {
         <div className="estereobckg">
           <img className="flex justify-around img-small center-img" src="/icons/projectlogos/estereo.png" alt=""/>
           <h2 className="white-center-text">A new Festival experience during COVID-19 times</h2>
-          <div className="flex justify-around px-16 mt-4">
-            <a href="/estereo"><button className="px-2 py-1 font-semibold text-white bg-transparent border rounded hover:bg-white-500 hover:text-white border-white-700 hover:border-transparent">Design Process</button></a>
-            <a href="/estereo"><button className="px-2 py-1 font-semibold text-white bg-transparent border rounded hover:bg-white-500 hover:text-white border-white-700 hover:border-transparent">Problem</button></a>
+          <div className="flex flex-wrap justify-around px-16 mt-4">
+            <a href="/estereo"><button className="px-2 py-1 mb-2 font-semibold text-white bg-transparent border rounded hover:bg-white-500 hover:text-white border-white-700 hover:border-transparent">Process</button></a>
+            <a href="/estereo"><button className="px-2 py-1 mb-2 font-semibold text-white bg-transparent border rounded hover:bg-white-500 hover:text-white border-white-700 hover:border-transparent">Problem</button></a>
             <a href="/estereo"><button className="px-2 py-1 font-semibold text-white bg-transparent border rounded hover:bg-white-500 hover:text-white border-white-700 hover:border-transparent">Solution</button></a>
           </div>
             <p className="mt-4 white-center-text">Responsive Design</p>
@@ -44,9 +44,9 @@ export default function projects() {
         <div className="bg-gray-400">
         <h2 className="pt-10 text-xl text-center text-black">Foodie experience</h2>
           <p className="text-center text-black">for latin american backpackers in Southeast Asia</p>
-          <div className="flex justify-around px-16 mt-4">
-            <a href="/backpackers"><button className="px-2 py-1 font-semibold text-black bg-transparent border border-black rounded hover:bg-white-500 hover:text-white hover:border-white">Design Process</button></a>
-            <a href="/backpackers"><button className="px-2 py-1 font-semibold text-black bg-transparent border border-black rounded hover:bg-white-500 hover:text-white hover:border-white">Problem</button></a>
+          <div className="flex flex-wrap justify-around px-16 mt-4">
+            <a href="/backpackers"><button className="px-2 py-1 mb-2 font-semibold text-black bg-transparent border border-black rounded hover:bg-white-500 hover:text-white hover:border-white">Design Process</button></a>
+            <a href="/backpackers"><button className="px-2 py-1 mb-2 font-semibold text-black bg-transparent border border-black rounded hover:bg-white-500 hover:text-white hover:border-white">Problem</button></a>
             <a href="/backpackers"><button className="px-2 py-1 font-semibold text-black bg-transparent border border-black rounded hover:bg-white-500 hover:text-white hover:border-white">Solution</button></a>
           </div>
             <p className="mt-4 text-center text-black">Mobile App</p>
