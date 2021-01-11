490123b7967f7f2f2613b6cc4a55011e57949996
paola1234 <paolalat188@gmail.com>
2021-01-10 22:35:12 -0500

    University Thesis prototype

--- pages/aboutme.js
+++ pages/aboutme.js
@@ -59,24 +59,33 @@ export default function aboutme() {
         </div>
       </section>
 
-      <section className="my-8">
+      <section className="my-12">
         <h2 className="text-xl font-extrabold"> My university Thesis</h2>
-        
-        <p> This thesis proposed to explore the viability of creating a mobile application that can provide a service oriented
-          towards the guidance of developing abilities for personal growth of the user. Currently, the tendency is 
+        <p className="mb-2"> This thesis proposed to explore the viability of creating a mobile application that can provide a service oriented
+          towards the guidance of developing abilities for personal growth of the user. </p>
+          <p className="mb-2">Currently, the tendency is 
           to hire a coach, but that is not the only solution, because of that, what is expected in the project is to understand
-          a way to connect the personal trainers through technology, using different educational technics different from only coaching. 
-          The market segmentation that we expect to reach with the project is people with 1 to 5 years of professional 
+          a way to connect the personal trainers through technology, using different educational technics different from only coaching. </p>
+          <p>The market segmentation that I expect to reach with the project is people with 1 to 5 years of professional 
           experience. </p>
-        <a  className="flex flex-wrap justify-around px-4 py-2" href="https://documentodegrado.uniandes.edu.co/acepto201699.php?id=20640.pdf">
+
+          <div className="flex flex-wrap justify-around px-8 mt-4">
+          <a  className="flex flex-wrap justify-around w-1/2 px-4 py-2" href="https://documentodegrado.uniandes.edu.co/acepto201699.php?id=20640.pdf">
               <button className="flex flex-wrap justify-around px-4 py-2 mx-auto font-bold text-white transform bg-purple-700 rounded hover:scale-105 hover:bg-purple-500">
-               Explore my thesis document here
+               Explore the thesis document
               </button>
           </a>
+          <a  className="flex flex-wrap justify-around w-1/2 px-4 py-2" href="https://xd.adobe.com/view/5282f035-f4c7-4547-7d5c-7767a6f722b7-24b3/">
+              <button className="flex flex-wrap justify-around px-4 py-2 mx-auto font-bold text-white transform bg-purple-700 rounded hover:scale-105 hover:bg-purple-500">
+               Explore the prototype
+              </button>
+          </a>
+          </div>
+
       </section>
       <section>
-            <h3 className="mb-2 center-text">If you want to know more about my experience</h3>
-            <p className="pb-2 text-center">I've designed and developed web pages for startups and NGOs. In my CV you can find the latest 2 experiences in web design.</p>
+            <h3 className="mt-10 mb-2">If you want to know more about my experience</h3>
+            <p className="pb-2 ">I've designed and developed web pages for startups and NGOs. In my CV you can find the latest 2 experiences in web design.</p>
             <a  className="flex flex-wrap justify-around px-4 py-2" href="https://drive.google.com/file/d/1JbFLHALH9IVVdv2cTt4tnLEZYOU-yDEd/view?usp=sharing">
               <button className="flex flex-wrap justify-around px-4 py-2 mx-auto font-bold text-white transform bg-purple-700 rounded hover:scale-105 hover:bg-purple-500">
                Download my CV
@@ -84,7 +93,7 @@ export default function aboutme() {
             </a>
       </section>
       <section>
-            <h3 className="pt-6 center-text">Contact me here</h3>
+            <h3 className="pt-6 mt-10 center-text">Contact me here</h3>
             <div className="rowMedia">
             <a href="https://www.behance.net/paola-latino" ><img className="mediaIcons" src="/icons/socialMedia/behance.svg" alt=""/></a>
             <a href="https://www.linkedin.com/in/paola-latino/"><img className="mediaIcons"src="/icons/socialMedia/linkedin.svg" alt=""/></a>  
