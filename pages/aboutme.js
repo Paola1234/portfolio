677560f92aacf701e83063e7564ee62f487b23c5
paola1234 <paolalat188@gmail.com>
2021-01-10 20:52:54 -0500

    University Thesis

--- pages/aboutme.js
+++ pages/aboutme.js
@@ -14,8 +14,7 @@ export default function aboutme() {
         <h2 className="center-title"> About Me </h2>
         <p className="mb-4 text-center">Let me tell you a few facts about me</p>
         <p className="mb-4">
-        I’m a computer engineer and designer whose 
-        love for tech and creativity who questions everyday “How can I make people’s life easier?”
+        I’m a computer engineer (by night) and designer (by day) whose love for tech and creativity who questions everyday “How can I make people’s life easier?”
         </p>
         <p className="mb-4">
         That’s why I create experiences through tech. I'm continuously learning about topics related 
@@ -58,6 +57,22 @@ export default function aboutme() {
                 </ul>
             </div>
         </div>
+      </section>
+
+      <section className="my-8">
+        <h2 className="text-xl font-extrabold"> My university Thesis</h2>
+        
+        <p> This thesis proposed to explore the viability of creating a mobile application that can provide a service oriented
+          towards the guidance of developing abilities for personal growth of the user. Currently, the tendency is 
+          to hire a coach, but that is not the only solution, because of that, what is expected in the project is to understand
+          a way to connect the personal trainers through technology, using different educational technics different from only coaching. 
+          The market segmentation that we expect to reach with the project is people with 1 to 5 years of professional 
+          experience. </p>
+        <a  className="flex flex-wrap justify-around px-4 py-2" href="https://documentodegrado.uniandes.edu.co/acepto201699.php?id=20640.pdf">
+              <button className="flex flex-wrap justify-around px-4 py-2 mx-auto font-bold text-white transform bg-purple-700 rounded hover:scale-105 hover:bg-purple-500">
+               Explore my thesis document here
+              </button>
+          </a>
       </section>
       <section>
             <h3 className="mb-2 center-text">If you want to know more about my experience</h3>
